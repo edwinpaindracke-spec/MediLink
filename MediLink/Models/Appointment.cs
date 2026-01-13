2592b6f7573e7bbf4ec6be26fb0bbd728ad1fa20
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2026-01-13 08:53:28 +0200

    creating a doctor schedule model and book appointment model

---
+++ MediLink/Models/Appointment.cs
@@ -0,0 +1,8 @@
+﻿public class Appointment
+{
+    public int Id { get; set; }
+    public string PatientId { get; set; }
+    public int DoctorId { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public string Status { get; set; }
+}
---
+++ MediLink/Models/DoctorSchedule.cs
@@ -0,0 +1,8 @@
+﻿public class DoctorSchedule
+{
+    public int Id { get; set; }
+    public int DoctorId { get; set; }
+    public DateTime AvailableDate { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+}
