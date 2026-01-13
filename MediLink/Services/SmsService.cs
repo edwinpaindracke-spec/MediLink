138bb22ad948bc7df2cb95318041a39d89fb998a
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2026-01-13 09:21:20 +0200

    creating a service folder and an sms service class for sending sms to the patients

---
+++ MediLink/Services/SmsService.cs
@@ -0,0 +1,28 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Microsoft.Extensions.Configuration;
+
+public class SmsService
+{
+    private readonly IConfiguration _config;
+
+    public SmsService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public void SendSms(string phone, string message)
+    {
+        var accountSid = _config["Twilio:AccountSid"];
+        var authToken = _config["Twilio:AuthToken"];
+        var fromPhone = _config["Twilio:FromPhone"];
+
+        TwilioClient.Init(accountSid, authToken);
+
+        MessageResource.Create(
+            body: message,
+            from: new Twilio.Types.PhoneNumber(fromPhone),
+            to: new Twilio.Types.PhoneNumber(phone)
+        );
+    }
+}
