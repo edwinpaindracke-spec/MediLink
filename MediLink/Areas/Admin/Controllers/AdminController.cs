2829b2edabfd0d510ec5fd39bfa67c8541918e0f
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2026-01-13 12:56:55 +0200

    creating the admin controller and areas folders for doctors and appointments also designing the welcome page

---
+++ MediLink/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediLink.Areas.Admin.Controllers
+{
+    // Tell ASP.NET Core this controller belongs to the Admin area
+    [Area("Admin")]
+
+    // Only users in the "Admin" role can access any action here
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        // Admin dashboard homepage
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
